0ae8c33802a8885d66c354ed8c2f9d4d169001b1
W4J1e <74824162+W4J1e@users.noreply.github.com>
2020-12-20 11:09:12 +0800

    Delete tqjs.js

--- js/tqjs.js
+++
@@ -1,5 +0,0 @@
-<script language="JavaScript">
-<!--
-document.writeln("<iframe width=/"320/" height=/"85/" frameborder=/"0/" scrolling=/"no/" hspace=/"0/" src=/"https://i.tianqi.com/?c=code&a=getcode&id=6&py=chongqing&icon=1/"></iframe>");
-//-->
-</script>
